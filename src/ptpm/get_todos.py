0e301b68d3f8c6102f75f6b8b372b9c9db416ef0
Calvin Loveland <calvin@loveland.dev>
2023-12-31 15:57:02 -0700

    Add .gitignore and TODO.txt files, update project name and version, and add test for coallate_todos function

--- src/ptpm/get_todos.py
+++ src/ptpm/get_todos.py
@@ -3,37 +3,48 @@ import os
 from git import Repo
 
 
-def get_todos():
-    file_types = [".py", ".html", ".js", ".css", ".md", ".txt"]
-
+def find_git_path_or_none():
     project_root = os.path.dirname(os.getcwd())
-    search_path = project_root
     git_path = os.path.join(project_root, ".git")
+    if os.path.exists(git_path):
+        return git_path
+    return None
+
+
+def find_relavent_files(search_path):
+    file_types = [".py", ".html", ".js", ".css", ".md", ".txt"]
+    git_path = find_git_path_or_none()
     print(f"Searching {search_path} for TODOs")
-    todos = []
 
     files_to_search = []
-    for root, dirs, files in os.walk(search_path):
+    for root, _dirs, files in os.walk(search_path):
         for file in files:
             if file not in files_to_search:
                 if any(file.endswith(file_type) for file_type in file_types):
                     files_to_search.append(os.path.join(root, file))
 
-    ignored_files = Repo(git_path).ignored(files_to_search)
-    file_to_search = filter(lambda x: x not in ignored_files, files_to_search)
+    if git_path:
+        ignored_files = Repo(git_path).ignored(files_to_search)
+        files_to_search = filter(lambda x: x not in ignored_files, files_to_search)
+    return files_to_search
 
-    for file in file_to_search:
-        if any(file.endswith(file_type) for file_type in file_types):
-            with open(os.path.join(file), "r", encoding="utf-8") as f:
-                lines = f.readlines()
-            i = 0
-            for line in lines:
-                i += 1
-                if "TODO (" in line:
-                    todo_string = line[line.find("TODO") + 4 :].strip()
-                    todos.append(f"{todo_string} location:{file} line_number:{i}")
-                if "TODO" in file:
-                    todos.append(line.strip())
+
+def get_todos():
+    todos = []
+    search_path = os.path.dirname(os.getcwd())
+    files_to_search = find_relavent_files(search_path)
+
+    for file in files_to_search:
+        with open(os.path.join(file), "r", encoding="utf-8") as f:
+            lines = f.readlines()
+        i = 0
+        for line in lines:
+            i += 1
+            if "TODO (" in line:
+                todo_string = line[line.find("TODO") + 4 :].strip()
+                todos.append(f"{todo_string} location:{file} line_number:{i}")
+            if "TODO" in file:
+                todos.append(line.strip())
 
     def custom_sorted(todos):
         return sorted(todos, key=lambda todo: todo.replace(")", "~"))
@@ -51,8 +62,9 @@ def print_todos():
 def coallate_todos():
     todos = get_todos()
     project_root = os.path.dirname(os.getcwd())
+    todo_file = None
     # Find any existing TODO files
-    for root, dirs, files in os.walk(os.path.join(project_root, "src")):
+    for root, _dirs, files in os.walk(os.path.join(project_root, "src")):
         for file in files:
             if file == "TODO.txt":
                 todo_file = os.path.join(root, file)
